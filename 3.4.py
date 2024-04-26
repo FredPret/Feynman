96ca5b118dd93855965288d1752e49cfb39da397
fred <fredpret@protonmail.ch>
2024-04-26 14:48:51 -0600

    3.4.py

---
+++ 3.4.py
@@ -0,0 +1,28 @@
+import sys
+from common.common import *
+
+
+def main() -> float:
+    # Earth
+    earth_diameter = 12756 * 1e3 # meters
+    earth_radius = earth_diameter / 2 # meters
+    earth_moon_mass_ratio = 81.3
+    earth_g = 9.81
+
+    # moon
+    moon_radius = 1738 * 1e3 # meters
+    moon_g = ( (1 / earth_moon_mass_ratio) * (earth_radius / moon_radius)**2 ) * earth_g
+
+    answer_dict = {
+        'moon_g': moon_g
+        }
+    
+    for k, v in answer_dict.items():
+        print(f"{k}: {v}")
+
+    answer = moon_g
+    
+    return answer
+
+if __name__ == '__main__':
+    sys.exit(main())
