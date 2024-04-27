c6edeaf84ed0d9fdb81f31c32e77fba5fac9f6ea
Fred <fred@protonmail.ch>
2024-04-27 23:07:18 +0000

    4.15

---
+++ 4.16.py
@@ -0,0 +1,29 @@
+import sys
+from common.common import *
+
+# test
+# test
+def main() -> float:
+    d = 22.5 # meters
+    
+    vy0 = 0
+    vyend = 0
+
+    g = -9.81
+
+    
+    #maxheight = vy0 + 0.5*g*tmaxy
+    tmaxy = (maxheight - vy0) / (0.5*g)
+
+    answer_dict = {
+        'moon_g': moon_g
+        }
+
+    for k, v in answer_dict.items():
+        print(f"{k}: {v}")
+
+    answer = moon_g
+    return answer
+
+if __name__ == '__main__':
+    sys.exit(main())
